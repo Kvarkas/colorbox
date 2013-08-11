3dceaa54c9b9d02cc2b0f85be1df3834dc1f2595
Kvarkas <kvarkas@gmail.com>
2013-08-11 12:45:54 +0100

    Add Lithuanian (LT) translation
    Lithuanian translation

---
+++ i18n/jquery.colorbox-lt.js
@@ -0,0 +1,16 @@
+/*
+  jQuery Colorbox language configuration
+	language: Lithuanian (lt)
+	translated by: Dimitrij Gorodeckij aka Kvark
+	site: http://www.polkan.eu/
+*/
+jQuery.extend(jQuery.colorbox.settings, {
+	current: "paveikslėlis {current} iš {total}",
+	previous: "ankstesnis",
+	next: "sekantis",
+	close: "uždaryti",
+	xhrError: "Nepavyko parsiųsti turinio.",
+	imgError: "Nepavyko parsiųsti paveikslėlio.",
+	slideshowStart: "pradėti automatinė peržiūrą",
+	slideshowStop: "stabdyti automatinė peržiūrą"
+});
